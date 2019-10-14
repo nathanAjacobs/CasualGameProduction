d81a3ddbe58e915f3271d2542ee899bd5fe0769f
Stormbringer12 <brenton.bushnell@gmail.com>
2019-10-14 09:36:01 -0400

    UI Functionality

---
+++ New Unity Project/Assets/Scripts/TrackColliderTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackColliderTrigger : MonoBehaviour
+{
+    public GameObject myPrefab;
+    public GameObject right;
+    public Transform newPos;
+
+    public static int index = 0;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            //Debug.Log("Yoooo");
+
+            if(index == 0)
+            {
+                GameObject temp = Instantiate(myPrefab, newPos.position, Quaternion.Euler(newPos.rotation.eulerAngles.x, -newPos.rotation.eulerAngles.y, newPos.rotation.eulerAngles.z));
+                index++;
+            }
+            else
+            {
+                GameObject temp = Instantiate(right, newPos.position, Quaternion.Euler(newPos.rotation.eulerAngles.x, -newPos.rotation.eulerAngles.y, newPos.rotation.eulerAngles.z));
+                index = 0;
+            }
+            
+
+
+
+            //temp.transform.position = GetComponentInChildren<Transform>().position;
+
+            //Debug.Log(GetComponentInChildren<Transform>().gameObject);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/TrackRotationScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackRotationScript : MonoBehaviour
+{
+
+    public int trackDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (trackDirection == -1)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, -50, transform.rotation.eulerAngles.z);
+        }
+        else if (trackDirection == 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 50, transform.rotation.eulerAngles.z);
+        }
+    }
+}
